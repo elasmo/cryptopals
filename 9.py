612edfe9fba2fe351450e350f8895ac50ee33bd5
elasmo <johan@jails.se>
2017-11-11 13:10:55 +0100

    Minor edit

--- 9.py
+++ 9.py
@@ -2,5 +2,9 @@
 #
 # Challenge 9: Implement PKCS #7 padding
 #
+from cryptopals import pkcs7_padding
 
+message = "YELLOW SUBMARINE"
+block_size = 20
 
+print(pkcs7_padding(message, block_size))
--- cryptopals.py
+++ cryptopals.py
@@ -23,152 +23,162 @@ freq_en = {
 
 # Perform frequency analysis and correlate with english language freq and return score
 def calc_score(freqs):
-	score = 0
-	for char, occur in freqs.items():
-		score += freq_en[char] + occur
+    score = 0
+    for char, occur in freqs.items():
+    	score += freq_en[char] + occur
 
-	return score
+    return score
 
 # Count ascii letters im a string
 def count_chars(text):
-	freqs = dict.fromkeys(ascii_lowercase, 0)
-	freqs.update({' ' : 0})
-	
-	for c in text.lower():
-		val = ord(c)
-		if val >= ord('a') and val <= ord('z') or val == ord(' '):
-			freqs[c] += 1
-
-	return freqs
+    freqs = dict.fromkeys(ascii_lowercase, 0)
+    freqs.update({' ' : 0})
+    
+    for c in text.lower():
+        val = ord(c)
+        if val >= ord('a') and val <= ord('z') or val == ord(' '):
+            freqs[c] += 1
+            
+    return freqs
 
 # Guess key length
 def guess_keylen(ciphertext, minlen, maxlen):
-	hamdist_avg_old = maxlen
-	guessed_keylen = 0
+    hamdist_avg_old = maxlen
+    guessed_keylen = 0
+    
+    for keylen in range(minlen, maxlen+1):
+        offset_start = keylen
+        offset_end = offset_start * 2
 
-	for keylen in range(minlen, maxlen+1):
-		offset_start = keylen
-		offset_end = offset_start * 2
+	# The length of n bytes with smallest hamming distance is probably
+	# the length of the key.
+	# Compare the first block of n bytes with the follow blocks and
+	# calculate an average.
 
-		# The length of n bytes with smallest hamming distance is probably
-		# the length of the key.
-		# Compare the first block of n bytes with the follow blocks and
-		# calculate an average.
+        hamdist_list = []
+        max_blocks = round(len(ciphertext)/keylen, 0)
 
-		hamdist_list = []
-		max_blocks = round(len(ciphertext)/keylen, 0)
+        while offset_end/keylen < max_blocks:
+            blk_one = ciphertext[:keylen]
+            blk_two = ciphertext[offset_start:offset_end]
+            
+            offset_start = offset_end
+            offset_end += keylen
 
-		while offset_end/keylen < max_blocks:
-			blk_one = ciphertext[:keylen]
-			blk_two = ciphertext[offset_start:offset_end]
+            hamdist_list.append(ham_distance(blk_one, blk_two)/keylen)
 
-			offset_start = offset_end
-			offset_end += keylen
+	    # Calculate an average hamming distance for the current key length
+            hamdist_avg = sum(hamdist_list)/len(hamdist_list)
 
-			hamdist_list.append(ham_distance(blk_one, blk_two)/keylen)
+        if(hamdist_avg < hamdist_avg_old):
+            guessed_keylen = keylen
+            hamdist_avg_old = hamdist_avg
 
-		# Calculate an average hamming distance for the current key length
-		hamdist_avg = sum(hamdist_list)/len(hamdist_list)
-		if(hamdist_avg < hamdist_avg_old):
-			guessed_keylen = keylen
-			hamdist_avg_old = hamdist_avg
-
-	return guessed_keylen
+    return guessed_keylen
 
 # Calculate hamming distance between two strings
 def ham_distance(str1, str2):
-	if len(str1) != len(str2):
-		raise ValueError("Strings not of equal length")
+    if len(str1) != len(str2):
+        raise ValueError("Strings not of equal length")
 
-	distance = 0
-	for a, b in zip(str1, str2):
-		distance += bin(a^b).count('1')
+    distance = 0
+    for a, b in zip(str1, str2):
+    	distance += bin(a^b).count('1')
 
-	return distance
+    return distance
 
 # Convert a hexadecimal string to base64
 def hexstr_to_b64(text):
-	return base64.b64encode(binascii.unhexlify(text))
+    return base64.b64encode(binascii.unhexlify(text))
 
 # Take two equal-length buffers and produce their XOR combination
 def hexstr_xor(str1, str2):
-	if len(str1) != len(str2):
-		raise ValueError("Strings not of equal length")
+    if len(str1) != len(str2):
+    	raise ValueError("Strings not of equal length")
 
-	hex_one = binascii.unhexlify(str1)
-	hex_two = binascii.unhexlify(str2)
+    hex_one = binascii.unhexlify(str1)
+    hex_two = binascii.unhexlify(str2)
 
-	result = ""
-	for c in range(len(hex_one)):
-		result += chr(hex_one[c]^hex_two[c])
+    result = ""
+    for c in range(len(hex_one)):
+    	result += chr(hex_one[c]^hex_two[c])
 
-	# str.encode returns a utf-8 encoded (default) bytes object
-	return binascii.hexlify(str.encode(result))
+    # str.encode returns a utf-8 encoded (default) bytes object
+    return binascii.hexlify(str.encode(result))
 
 # Split a string into blocks
 def split_string(text, block_size):
-	return [text[i:i+block_size] for i in range(0, len(text), block_size)]
+    return [text[i:i+block_size] for i in range(0, len(text), block_size)]
 
 # Transpose blocks: make a block that is the first byte of every block, 
 # and a block that is the second byte of every block, and so on. 
 def transp_blks(blks):
-	transp_list = []
+    transp_list = []
 
-	keylen = len(blks[0])
-	for char in range(keylen):
-		buf = bytes()
-		for i in range(len(blks)):
-			buf += blks[i][char:char+1]
+    keylen = len(blks[0])
+    for char in range(keylen):
+        buf = bytes()
+        for i in range(len(blks)):
+            buf += blks[i][char:char+1]
 
-		transp_list.append(buf)
+        transp_list.append(buf)
 
-	return transp_list
+    return transp_list
 
 # Repeating-XOR decipher
 def repeating_key_xor(ciphertext, key):
-	if len(ciphertext) != len(key):
-		# XXX: should I use padding instead(?)
-		key = key[0:len(ciphertext)]
+    if len(ciphertext) != len(key):
+    	# XXX: should I use padding instead(?)
+    	key = key[0:len(ciphertext)]
 	
-	buf = ""
-	for m, k in zip(key, ciphertext):
-		if type(m) is str:
-			m = ord(m)
-		if type(k) is str:
-			k = ord(k)
+    buf = ""
+    for m, k in zip(key, ciphertext):
+        if type(m) is str:
+            m = ord(m)
+        if type(k) is str:
+            k = ord(k)
 
-		buf += chr(m^k)
+        buf += chr(m^k)
 
-	return buf
+    return buf
 
 
 # Single byte XOR decipher
 def single_xor_dec(ciphertext, key):
-	plaintext = ""
+    plaintext = ""
+    
+    for i in range(len(ciphertext)):
+        if type(ciphertext[i]) is str:
+            ct = ord(ciphertext[i])
+        else:
+            ct = ciphertext[i]
+            
+        if type(key) is str:
+            key = ord(key)
+        
+        plaintext += chr(ct^key)
 
-	for i in range(len(ciphertext)):
-		if type(ciphertext[i]) is str:
-			ct = ord(ciphertext[i])
-		else:
-			ct = ciphertext[i]
-
-		if type(key) is str:
-			key = ord(key)
-			
-		plaintext += chr(ct^key)
-
-	return plaintext
+    return plaintext
 
 
 # Decrypt AES cipher in ECB mode
 def aes_ecb_dec(ciphertext, key):
-	cipher = AES.new(key, AES.MODE_ECB)
-	return cipher.decrypt(ciphertext)
-
-# Detect AES cipher in ECB mode
-def detect_aes_ecb(ciphertext):
-	return
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.decrypt(ciphertext)
 
 # PKCS#7 padding 
 def pkcs7_padding(block, block_size):
-    return
+    block_len = len(block)
+
+    if block is str:
+        block = bytes(block)
+
+    if block_len > block_size:
+        raise ValueError("Block length is larger than block size")
+
+    padding_len = block_size - (len(block) % block_size)
+    for p in range(padding_len):
+        block += bytes(padding_len)
+
+    print(padding_len)
+    return block
