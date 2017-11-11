d76c26c2a4744d08bedf5c472a89eb010e65d7ce
elasmo <johan@jails.se>
2017-11-11 13:46:56 +0100

    Minor edit

--- 9.py
+++ 9.py
@@ -6,6 +6,7 @@ from cryptopals import pkcs7_padding
 
 if __name__ == '__main__':
     message = "YELLOW SUBMARINE"
-    block_size = 20
 
-    print(pkcs7_padding(message, block_size))
+    for block_size in range(2,20):
+        padded_block = pkcs7_padding(message, block_size)
+        print(block_size, padded_block,"Block length:", len(padded_block))
