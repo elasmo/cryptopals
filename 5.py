91b140d16be9bf0a05f056c306321f21f71b4f04
elasmo <johan@jails.se>
2017-11-17 09:25:43 +0100

    fixed indentation

--- 5.py
+++ 5.py
@@ -8,22 +8,22 @@ from cryptopals import repeating_key_xor
 import binascii
 
 if len(argv) <= 1:
-	print("Usage: {} <filename>".format(basename(argv[0])))
-	exit(0)
+    print("Usage: {} <filename>".format(basename(argv[0])))
+    exit(0)
 
 ciphertext = ""
 filename = argv[1]
 key = "ICE"
 
 with open(filename, 'rb') as fo:
-	while True:
-		chunk = fo.read(len(key))
+    while True:
+        chunk = fo.read(len(key))
 
-		if not chunk:
-			break
+        if not chunk:
+            break
 
-		print("Processing '" + str(chunk) + "'")
-		ciphertext += repeating_key_xor(chunk, key)
+	print("Processing '" + str(chunk) + "'")
+        ciphertext += repeating_key_xor(chunk, key)
 
 print("Encrypted:")
 print(''.join('{:02x}'.format(ord(c)) for c in ciphertext))
