6645c9f1ab1787751b06160e244640e8fcf9225e
elasmo <johan@jails.se>
2017-11-19 22:49:25 +0100

    clean up

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@
 from cryptopals import hexstr_xor
 
 if __name__ == "__main__":
-    buf1 = "1c0111001f010100061a024b53535009181c"
-    buf2 = "686974207468652062756c6c277320657965"
+    buf1 = b"1c0111001f010100061a024b53535009181c"
+    buf2 = b"686974207468652062756c6c277320657965"
 
     print(hexstr_xor(buf1, buf2))
